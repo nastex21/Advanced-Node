bc0973f01c9240a6b4cb2822c3cdba7255b56dfc
Tony <nasax2000@gmail.com>
2018-12-03 23:33:33 -0600

    Commit

--- server.js
+++ server.js
@@ -75,14 +75,28 @@ mongo.connect(process.env.MONGO_URI, (err, db) => {
 
     app.route('/profile')
       .get(ensureAuthenticated, (req, res) => {
-        res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
+        res.render(process.cwd() + '/views/pug/profile', {
+          username: req.user.username
+        });
       });
 
     app.route('/login').post(passport.authenticate('local', {
       failureRedirect: '/'
     }), (req, res) => {
       res.redirect('/profile');
-    })
+    });
+
+    app.route('/logout')
+      .get((req, res) => {
+        req.logout();
+        res.redirect('/');
+      });
+
+      app.use((req, res, next) => {
+        res.status(404)
+          .type('text')
+          .send('Not Found');
+      });
 
     app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + process.env.PORT);
