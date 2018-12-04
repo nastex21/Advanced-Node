f1c541637b99b793d4c613215e19aaabb1d3f650
Tony <nasax2000@gmail.com>
2018-12-04 11:47:42 -0600

    Commit

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const passport = require('passport');
+
 const session = require('express-session');
+const passport = require('passport');
 const mongo = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
 
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+const app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
 app.set('view engine', 'pug');
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -26,7 +26,11 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-mongo.connect(process.env.MONGO_URI, (err, db) => {
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongo.connect(process.env.MONGO_URI, (err, client) => {
+  var db = client.db('user-db');
   if (err) {
     console.log('Database error: ' + err);
   } else {
@@ -73,14 +77,25 @@ mongo.connect(process.env.MONGO_URI, (err, db) => {
       res.redirect('/');
     };
 
+    app.route('/')
+      .get((req, res) => {
+        res.render(process.cwd() + '/views/pug/index', {
+          title: 'Hello',
+          message: 'Please login',
+          showLogin: true,
+          showRegistration: true
+        })
+      });
+
     app.route('/profile')
-      .get(ensureAuthenticated, (req, res) => {
+      .get(ensureAuthenticated, (req, res) => {;
         res.render(process.cwd() + '/views/pug/profile', {
           username: req.user.username
         });
       });
 
-    app.route('/login').post(passport.authenticate('local', {
+    app.route('/login')
+    .post(passport.authenticate('local', {
       failureRedirect: '/'
     }), (req, res) => {
       res.redirect('/profile');
@@ -92,11 +107,45 @@ mongo.connect(process.env.MONGO_URI, (err, db) => {
         res.redirect('/');
       });
 
-      app.use((req, res, next) => {
-        res.status(404)
-          .type('text')
-          .send('Not Found');
-      });
+    app.route('/register')
+      .post((req, res, next) => {
+          db.collection('users').findOne({
+            username: req.body.username
+          }, function (err, user) {
+            if (err) {
+              next(err);
+            } else if (user) {
+              res.redirect('/');
+            } else {
+              db.collection('users').insertOne({
+                  username: req.body.username,
+                  password: req.body.password
+                },
+                (err, doc) => {
+                  if (err) {
+                    res.redirect('/');
+                  } else {
+                    next(null, user);
+                  }
+                }
+              )
+            }
+          })
+        },
+        passport.authenticate('local', {
+          failureRedirect: '/'
+        }),
+        (req, res, next) => {
+          res.redirect('/profile');
+        }
+      );
+
+
+    app.use((req, res, next) => {
+      res.status(404)
+        .type('text')
+        .send('Not Found');
+    });
 
     app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + process.env.PORT);
@@ -104,11 +153,3 @@ mongo.connect(process.env.MONGO_URI, (err, db) => {
 
   }
 });
-
-app.route('/')
-  .get((req, res) => {
-    res.render(process.cwd() + '/views/pug/index', {
-      title: 'Hello',
-      message: 'Please login'
-    })
-  });
