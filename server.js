4303598af4d7674a8b0b52549fe25286551af65c
Tony <nasax2000@gmail.com>
2018-12-03 22:01:15 -0600

    commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
 require('dotenv').config();
 const express     = require('express');
-var path = require('path');
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
 
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 app.use(express.urlencoded({extended: true}));
@@ -11,6 +12,14 @@ app.set('view engine', 'pug');
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
 
 app.route('/')
   .get((req, res) => {
