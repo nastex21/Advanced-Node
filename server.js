8689359912cbaa48db46f93b541ab0940d19300d
Tony <nasax2000@gmail.com>
2018-12-03 20:40:08 -0600

    Commit

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 'use strict';
-
+require('dotenv').config()
 const express     = require('express');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug');
+
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json);
